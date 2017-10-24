6b7c8168c85f271d79c2d9518a0f5eef78572094
nesrineayari <32552633+nesrineayari@users.noreply.github.com>
2017-10-24 10:28:01 +0100

    Add files via upload

---
+++ tp2.ex2.c
@@ -0,0 +1,25 @@
+/*
+ ============================================================================
+ Name        : ex2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+
+#include <stdio.h>
+int main (void)
+{
+	float x,s;
+printf("calcuer la valeur absolu de x:");
+scanf("%c",&x);
+if(x>=0)
+s=-x;
+else
+	s=x;
+return 0;
+
+}
+

2f1c2a07dacd3e4242df2a3445f95009f8dd97c0
nesrineayari <32552633+nesrineayari@users.noreply.github.com>
2017-10-24 10:35:11 +0100

    Add files via upload

---
+++ tp2.exe7.c
@@ -0,0 +1,35 @@
+/*
+ ============================================================================
+ Name        : exe7.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	char c;
+	printf("donner un caractere c");
+	scanf("%c",&c);
+	if(c>='a' && c<='z')
+		{printf("cette caractere et une lettre miniscule");}
+	else
+		if{(c>='A'&& c<='Z')
+			{printf("cette caractere et une lettre majiscule");}
+		}
+
+		else if
+		{
+			(c>=0 && c<=9)
+				{printf("cette caractere et un chiffre");}
+			}
+
+			else
+				{printf("cette unne autre caractere");}
+			return 0;
+
+}
